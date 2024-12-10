b67051014195a01b89c0f76ac19354b5a857d285
Chinmaya7064 <chinmayapanda445@gmail.com>
2024-12-11 00:02:48 +0530

    category actions added

--- controllers/admin/CategoryController.js
+++ controllers/admin/CategoryController.js
@@ -28,3 +28,70 @@ exports.postAddCategoryPage = (req, res) => {
             console.log(error)
         })
 }
+
+// Get Edit Category Page
+exports.getEditCategoryPage = (req, res) => {
+    const categoryId = req.params.categoryId;
+
+    let viewsData = {
+        edit: true,
+        pageTitle: 'Edit Category',
+    };
+
+    Category.findAll()  // Fetch all categories to display in the table
+        .then((categories) => {
+            // Find the specific category to edit
+            return Category.findByPk(categoryId)
+                .then((category) => {
+                    if (!category) {
+                        return res.redirect('/categories');
+                    }
+                    viewsData = { ...viewsData, category, categories }; // Add both category and categories to viewsData
+                    res.render('admin/categories/categoriesPage', viewsData);
+                });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+// Post Edit Category
+exports.postEditCategoryPage = (req, res) => {
+    const categoryId = req.body.categoryId;
+    const updatedTitle = req.body.title;
+
+    Category.findByPk(categoryId)
+        .then((category) => {
+            if (!category) {
+                return res.redirect('/categories');
+            }
+            category.title = updatedTitle;
+            return category.save();
+        })
+        .then(() => {
+            res.redirect('/categories');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+// Post Delete Category
+exports.postDeleteCategoryPage = (req, res) => {
+    const categoryId = req.body.categoryId;
+
+    Category.findByPk(categoryId)
+        .then((category) => {
+            if (!category) {
+                return res.redirect('/categories');
+            }
+            return category.destroy();
+        })
+        .then(() => {
+            res.redirect('/categories');
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const { getCategoryPage, getAddCategoryPage, postAddCategoryPage } = require('../controllers/admin/CategoryController');
+const { getCategoryPage, getAddCategoryPage, postAddCategoryPage, getEditCategoryPage, postEditCategoryPage, postDeleteCategoryPage } = require('../controllers/admin/CategoryController');
 
 const router = express.Router();
 
 router.get('/', getCategoryPage);
 router.get('/add', getAddCategoryPage);
 router.post('/add', postAddCategoryPage);
+router.get('/edit/:categoryId', getEditCategoryPage);
+router.post('/edit', postEditCategoryPage);
+router.post('/delete', postDeleteCategoryPage);
 
 module.exports = router;
